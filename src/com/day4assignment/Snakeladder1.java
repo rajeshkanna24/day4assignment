52d4501838201e17d5f164aa1a5fe4c4f6cca386
rajeshkanna24 <rajeshkanna2406gmail.com>
2022-12-19 19:01:33 +0530

    Add this file

---
+++ src/com/day4assignment/Snakeladder1.java
@@ -0,0 +1,11 @@
+package com.day4assignment;
+
+public class Snakeladder1 {
+    static final int startPosition = 0;
+    public static void main(String[] args) {
+            System.out.println("Welcome to snake and ladder program");
+            int position = startPosition;
+            System.out.println("current position : "+position);
+        }
+    }
+
