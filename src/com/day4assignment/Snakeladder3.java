cbb120133ea82ad4d3558f2b37135efa4ab2cdc9
rajeshkanna24 <rajeshkanna2406gmail.com>
2022-12-20 10:25:41 +0530

    add this Uc3 file

---
+++ src/com/day4assignment/Snakeladder3.java
@@ -0,0 +1,42 @@
+package com.day4assignment;
+import java.util.Random;
+public class Snakeladder3 {
+    static final int startPosition = 0;
+    static final int ladderOption = 2;
+    static final int snakeOption = 1;
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder program");
+        Random rnd = new Random();
+        int dieNumber, option;
+        int position = startPosition;
+
+        dieNumber = rnd.nextInt(1,7);
+        option = rnd.nextInt(1,3);
+        System.out.println("Die Number = "+dieNumber);
+
+        switch (option){
+            case ladderOption:
+                System.out.println("ladder option");
+                position += dieNumber;
+                break;
+            case snakeOption:
+                System.out.println("snake option");
+                position -= dieNumber;
+                if (position < 0)
+                {
+                    System.out.println("The player bit by snake");
+                    position = 0;
+                }
+                else
+                {
+                    System.out.println("The player bit by snake");
+                }
+                break;
+            default:
+                System.out.println("No Play option");
+                break;
+        }
+        System.out.println("current position : "+position);
+
+    }
+}
